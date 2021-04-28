3204a9bcc766205774f9b2a2ebc67bef72b99992
Daryna Prokopchuk <prokopchukdaryna0@gmail.com>
2021-04-28 15:13:16 +0400

    bug fixd

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Profile from './components/Profile/profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import user from './data/user.json';
+import stats from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transaction.json';
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={stats} />
+      <Statistics stats={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
